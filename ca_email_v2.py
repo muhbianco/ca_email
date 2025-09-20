4b15b3ba4308900e96b6b17d37589467ed4bb01a
Murilo Bianco <muhbianco@gmail.com>
2025-09-20 14:02:04 -0300

    new version

--- ca_email_v2.py
+++ ca_email_v2.py
@@ -6,6 +6,7 @@ import requests
 import tempfile
 import time
 import asyncio
+import aiohttp
 from imap_tools import MailBox, AND
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -21,127 +22,182 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PATH = os.getenv("EMAIL_PATH")
 NNHOOK_URL = os.getenv("N8N_WEBHOOK_URL")
 N8NAPI_KEY = os.getenv("N8N_API_KEY")
+CCAGIL_URL = os.getenv("CONTAAGIL_URL")
+
+CONTAAGIL_LOGIN = os.getenv("CONTAAGIL_LOGIN")
+CONTAAGIL_PASSW = os.getenv("CONTAAGIL_PASSW")
 
 local_time_fuse = pytz.timezone("America/Sao_Paulo")
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def get_msg_attachments(attachments):
-    attachs = []
-    if attachments:
-        for att in attachments:
-            attachs.append(att)
-    return attachs
+class CA_EMAIL:
+    def __init__(self):
+        pass
 
-def save_and_create_files(attachs):
-    files = []
-    with tempfile.TemporaryDirectory() as temp_dir:
-        for att in attachs:
-            file_path = os.path.join(temp_dir, att.filename)
-            with open(file_path, 'wb') as f:
-                f.write(att.payload)
-            files.append(('files', (att.filename, open(file_path, 'rb'), att.content_type)))
-    return files
+    async def log_response_error(self):
+        error_message = await self.response.text()
+        logging.error(f"Status Code: {self.response.status}")
+        logging.error(f"Error message: {error_message}")
 
-def message_extract(msg):
-    message = msg[:msg.find(".brevosend.com>")]
-    pattern = r"On \w{3}, \w+ \d{1,2}, \d{4} at \d{1,2}:\d{2}.*"
-    message = re.sub(pattern, '', message)
-    return message
-
-def internal_code_extract(html):
-    match = '<p class="CONTAAGIL-UUID" style="color:white;display:none">'
-    start_point = html.find(match)+len(match)
-    code = html[start_point:]
-    end_point = code.find("</p>")
-    code = code[:end_point]
-    return code
-
-def remove_html_tags(text):
-    soup = BeautifulSoup(text, "html.parser")
-    return soup.get_text()
-
-async def cod_lead_from_email(from_):
-    async with get_session() as db:
-        sql = "SELECT CodLead FROM Leads WHERE Email = %s"
-        response = await db.fetchone(sql, (from_, ))
-        if not response:
-            return None
-        return response["CodLead"]
-
-async def process_mailbox(msg):
-    date = re.sub(r"\s*\(.*?\)\s*", "", msg.date_str)
-    date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
-    date = date.astimezone(local_time_fuse)
-    date = date.strftime("%d-%m-%Y %H:%M:%S")
-
-    cod_lead = await cod_lead_from_email(msg.from_)
-    response_from_email = msg.from_
-    response_to_email = msg.to[0]
-    response_subject = msg.subject
-    response_message = message_extract(msg.text)
-
-    # logging.info(f"UID: {msg.uid}")
-    # logging.info(f"SUBJECT: {response_subject}")
-    # logging.info(f"FROM: {response_from_email}")
-    # logging.info(f"DATE: {date}")
-    # logging.info(f"BODY: {msg.text}")
-    # logging.info(f"htmlBODY: {msg.html}")
-    # logging.info(f"Código Interno: {internal_code}")
-    # logging.info(f"RESPONSE MESSAGE: {response_message}")
-
-    files = []
-    if msg.attachments:
-        attachs = get_msg_attachments(msg.attachments)
-        if attachs:
-            # logging.info(f"ATTACH: {', '.join([attach.filename for attach in attachs])}")
-            # logging.info("===================================\n\n")
-            files = save_and_create_files(attachs)
-
-    if cod_lead:
+    async def send_message_to_n8n(self):
         headers = {
             "Authorization": F"Bearer {N8NAPI_KEY}",
         }
         payload = {
-            "cod_lead": cod_lead,
-            "message": response_message,
-            "from_email": response_from_email,
-            "to_email": response_to_email,
-            "subject": response_subject,
+            "cod_lead": self.cod_lead,
+            "message": self.response_message,
+            "from_email": self.response_from_email,
+            "to_email": self.response_to_email,
+            "subject": self.response_subject,
             "incoming": True,
             "channel": "email",
         }
-        response = requests.post(NNHOOK_URL, headers=headers, data=payload, files=files)
-        if response.status_code != 200:
-            logging.error(response.text)
+        async with aiohttp.ClientSession() as session:
+            self.response = await session.post(NNHOOK_URL, headers=headers, json=payload)
 
+    async def contaagil_login(self):
+        headers = {
+            "Session-Token": "",
+        }
+        payload = {
+            "email": CONTAAGIL_LOGIN,
+            "password": CONTAAGIL_PASSW,
+        }
+        async with aiohttp.ClientSession() as session:
+            self.response = await session.post(f"{CCAGIL_URL}/Authenticator/getToken", headers=headers, json=payload)
 
+    async def send_files_to_db(self):
+        await self.contaagil_login()
+        if self.response.status != 200:
+            logging.error(f"Falha ao obter session_token ContaÁgil")
+            return
 
-async def main():
-    max_retries = 50
-    delay = 5
+        login_response = await self.response.json()
+        session_token = login_response.get("session_token")
+        headers = {
+            "Session-Token": session_token,
+        }
+        payload = {
+            "CodLead": self.cod_lead,
+            "saveFile": True,
+            "tipo": "ATTACH_LEAD_MESSAGE"
+        }
+        async with aiohttp.ClientSession() as session:
+            form = aiohttp.FormData()
+            for key, value in payload.items():
+                form.add_field(key, str(value))
+            for field_name, (filename, fileobj, content_type) in self.files:
+                form.add_field(
+                    field_name,
+                    fileobj,
+                    filename=filename,
+                    content_type=content_type,
+                )
+            self.response = await session.post(f"{CCAGIL_URL}/Upload/index", headers=headers, data=form)
+        
 
-    attempt = 0
-    while attempt < max_retries:
-        try:
-            with MailBox(EMAIL_HOST).login(EMAIL_USER, EMAIL_PASS, EMAIL_PATH) as mb:
-                while True:
-                    try:
-                        attempt = 0
-                        for msg in mb.fetch(AND(seen=False), mark_seen=True):
-                            await process_mailbox(msg)
-                    except Exception as e:
-                        logging.error(f"Ocorreu um erro: {e}")
-                        break
-        except Exception as e:
-            attempt += 1
-            logging.warning(f"Attempt {attempt}/{max_retries} Error: {e}")
-            if attempt < max_retries:
-                logging.info(f"Trying in {delay} seconds...")
-                await asyncio.sleep(delay)
-            else:
-                logging.error("Max tries. Out...")
+    def get_msg_attachments(self, attachments):
+        attachs = []
+        if attachments:
+            for att in attachments:
+                attachs.append(att)
+        return attachs
+
+    def save_and_create_files(self, attachs):
+        files = []
+        with tempfile.TemporaryDirectory() as temp_dir:
+            for att in attachs:
+                file_path = os.path.join(temp_dir, att.filename)
+                with open(file_path, 'wb') as f:
+                    f.write(att.payload)
+                files.append(('files', (att.filename, open(file_path, 'rb'), att.content_type)))
+        return files
+
+    def message_extract(self, msg):
+        message = msg[:msg.find(".brevosend.com>")]
+        pattern = r"On \w{3}, \w+ \d{1,2}, \d{4} at \d{1,2}:\d{2}.*"
+        message = re.sub(pattern, '', message)
+        return message
+
+    def internal_code_extract(self, html):
+        match = '<p class="CONTAAGIL-UUID" style="color:white;display:none">'
+        start_point = html.find(match)+len(match)
+        code = html[start_point:]
+        end_point = code.find("</p>")
+        code = code[:end_point]
+        return code
+
+    def remove_html_tags(self, text):
+        soup = BeautifulSoup(text, "html.parser")
+        return soup.get_text()
+
+    async def cod_lead_from_email(self, from_):
+        async with get_session() as db:
+            sql = "SELECT CodLead FROM Leads WHERE Email = %s"
+            response = await db.fetchone(sql, (from_, ))
+            if not response:
+                return None
+            return response["CodLead"]
+
+    async def process_mailbox(self, msg):
+        date = re.sub(r"\s*\(.*?\)\s*", "", msg.date_str)
+        date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
+        date = date.astimezone(local_time_fuse)
+        date = date.strftime("%d-%m-%Y %H:%M:%S")
+
+        self.cod_lead = await self.cod_lead_from_email(msg.from_)
+        self.response_from_email = msg.from_
+        self.response_to_email = msg.to[0]
+        self.response_subject = msg.subject
+        self.response_message = self.message_extract(msg.text)
+
+        if not self.cod_lead:
+            return
+
+        self.files = []
+        if msg.attachments:
+            attachs = self.get_msg_attachments(msg.attachments)
+            if attachs:
+                self.files = self.save_and_create_files(attachs)
+
+        await self.send_message_to_n8n()
+        if self.response.status != 200:
+            logging.error(f"Erro ao enviar mensagem para o N8N")
+            await self.log_response_error()
+            return
+
+        await self.send_files_to_db()
+        if self.response.status != 200:
+            logging.error(f"Erro ao enviar arquivos para o EP /Upload/index")
+            await self.log_response_error()
+            return
+
+    async def main(self):
+        max_retries = 50
+        delay = 5
+
+        attempt = 0
+        while attempt < max_retries:
+            try:
+                with MailBox(EMAIL_HOST).login(EMAIL_USER, EMAIL_PASS, EMAIL_PATH) as mb:
+                    while True:
+                        try:
+                            attempt = 0
+                            for msg in mb.fetch(AND(seen=False), mark_seen=True):
+                                await self.process_mailbox(msg)
+                        except Exception as e:
+                            logging.error(f"Ocorreu um erro: {e}")
+                            break
+            except Exception as e:
+                attempt += 1
+                logging.warning(f"Attempt {attempt}/{max_retries} Error: {e}")
+                if attempt < max_retries:
+                    logging.info(f"Trying in {delay} seconds...")
+                    await asyncio.sleep(delay)
+                else:
+                    logging.error("Max tries. Out...")
 
 if __name__ == "__main__":
-    version = "v1.2"
+    version = "v1.3"
     logging.info(f"[CA EMAIL Version {version} starded.]")
-    asyncio.run(main())
+    asyncio.run(CA_EMAIL().main())
